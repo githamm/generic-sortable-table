4f24d23bdfbce3e9b7c43ecb3c2bac9b983a37d5
githamm <hamm.kevin@gmail.com>
2015-10-21 11:22:39 -0600

    First commit

---
+++ unemployment-data.js
@@ -0,0 +1,578 @@
+[
+  {
+    "county": "Denver",
+    "laborForce": 370916,
+    "employed": 358871,
+    "unemployed": 12045,
+    "rate": 3.2,
+    "lastLow": "10-2000",
+    "lastLowYear": 2000
+  },
+  {
+    "county": "Arapahoe",
+    "laborForce": 333586,
+    "employed": 322828,
+    "unemployed": 10758,
+    "rate": 3.2,
+    "lastLow": "5-2001",
+    "lastLowYear": 2001
+  },
+  {
+    "county": "Jefferson",
+    "laborForce": 312542,
+    "employed": 303043,
+    "unemployed": 9499,
+    "rate": 3,
+    "lastLow": "5-2001",
+    "lastLowYear": 2001
+  },
+  {
+    "county": "El Paso",
+    "laborForce": 302558,
+    "employed": 290542,
+    "unemployed": 12016,
+    "rate": 4,
+    "lastLow": "5-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Adams",
+    "laborForce": 245278,
+    "employed": 236061,
+    "unemployed": 9217,
+    "rate": 3.8,
+    "lastLow": "5-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Larimer",
+    "laborForce": 179510,
+    "employed": 174590,
+    "unemployed": 4920,
+    "rate": 2.7,
+    "lastLow": "5-2001",
+    "lastLowYear": 2001
+  },
+  {
+    "county": "Boulder",
+    "laborForce": 176996,
+    "employed": 172333,
+    "unemployed": 4663,
+    "rate": 2.6,
+    "lastLow": "7-2000",
+    "lastLowYear": 2000
+  },
+  {
+    "county": "Douglas",
+    "laborForce": 169556,
+    "employed": 165089,
+    "unemployed": 4467,
+    "rate": 2.6,
+    "lastLow": "5-2001",
+    "lastLowYear": 2001
+  },
+  {
+    "county": "Weld",
+    "laborForce": 151036,
+    "employed": 146090,
+    "unemployed": 4946,
+    "rate": 3.3,
+    "lastLow": "5-2001",
+    "lastLowYear": 2001
+  },
+  {
+    "county": "Mesa",
+    "laborForce": 73345,
+    "employed": 69828,
+    "unemployed": 3517,
+    "rate": 4.8,
+    "lastLow": "Rising",
+    "lastLowYear": "Rising"
+  },
+  {
+    "county": "Pueblo",
+    "laborForce": 72120,
+    "employed": 68450,
+    "unemployed": 3670,
+    "rate": 5.1,
+    "lastLow": "12-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Broomfield",
+    "laborForce": 33746,
+    "employed": 32768,
+    "unemployed": 978,
+    "rate": 2.9,
+    "lastLow": "5-2001",
+    "lastLowYear": 2001
+  },
+  {
+    "county": "Eagle",
+    "laborForce": 30962,
+    "employed": 30209,
+    "unemployed": 753,
+    "rate": 2.4,
+    "lastLow": "8-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Garfield",
+    "laborForce": 30110,
+    "employed": 29117,
+    "unemployed": 993,
+    "rate": 3.3,
+    "lastLow": "10-2008",
+    "lastLowYear": 2008
+  },
+  {
+    "county": "La Plata",
+    "laborForce": 29408,
+    "employed": 28569,
+    "unemployed": 839,
+    "rate": 2.9,
+    "lastLow": "12-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Montrose",
+    "laborForce": 19326,
+    "employed": 18537,
+    "unemployed": 789,
+    "rate": 4.1,
+    "lastLow": "12-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Summit",
+    "laborForce": 18655,
+    "employed": 18240,
+    "unemployed": 415,
+    "rate": 2.2,
+    "lastLow": "2-2001",
+    "lastLowYear": 2001
+  },
+  {
+    "county": "Morgan",
+    "laborForce": 14837,
+    "employed": 14346,
+    "unemployed": 491,
+    "rate": 3.3,
+    "lastLow": "11-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Fremont",
+    "laborForce": 14440,
+    "employed": 13688,
+    "unemployed": 752,
+    "rate": 5.2,
+    "lastLow": "12-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Delta",
+    "laborForce": 13438,
+    "employed": 12771,
+    "unemployed": 667,
+    "rate": 5,
+    "lastLow": "11-2008",
+    "lastLowYear": 2008
+  },
+  {
+    "county": "Routt",
+    "laborForce": 13414,
+    "employed": 13063,
+    "unemployed": 351,
+    "rate": 2.6,
+    "lastLow": "2-2008",
+    "lastLowYear": 2008
+  },
+  {
+    "county": "Elbert",
+    "laborForce": 13171,
+    "employed": 12835,
+    "unemployed": 336,
+    "rate": 2.6,
+    "lastLow": "5-2001",
+    "lastLowYear": 2001
+  },
+  {
+    "county": "Montezuma",
+    "laborForce": 12230,
+    "employed": 11663,
+    "unemployed": 567,
+    "rate": 4.6,
+    "lastLow": "9-2008",
+    "lastLowYear": 2008
+  },
+  {
+    "county": "Teller",
+    "laborForce": 11464,
+    "employed": 11043,
+    "unemployed": 421,
+    "rate": 3.7,
+    "lastLow": "10-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Logan",
+    "laborForce": 11010,
+    "employed": 10707,
+    "unemployed": 303,
+    "rate": 2.8,
+    "lastLow": "5-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Pitkin",
+    "laborForce": 10471,
+    "employed": 10188,
+    "unemployed": 283,
+    "rate": 2.7,
+    "lastLow": "4-2008",
+    "lastLowYear": 2008
+  },
+  {
+    "county": "Gunnison",
+    "laborForce": 9648,
+    "employed": 9436,
+    "unemployed": 212,
+    "rate": 2.2,
+    "lastLow": "New Low",
+    "lastLowYear": "New low"
+  },
+  {
+    "county": "Park",
+    "laborForce": 9461,
+    "employed": 9187,
+    "unemployed": 274,
+    "rate": 2.9,
+    "lastLow": "5-2001",
+    "lastLowYear": 2001
+  },
+  {
+    "county": "Grand",
+    "laborForce": 8713,
+    "employed": 8487,
+    "unemployed": 226,
+    "rate": 2.6,
+    "lastLow": "9-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Chaffee",
+    "laborForce": 8547,
+    "employed": 8310,
+    "unemployed": 237,
+    "rate": 2.8,
+    "lastLow": "8-2000",
+    "lastLowYear": 2000
+  },
+  {
+    "county": "Otero",
+    "laborForce": 7836,
+    "employed": 7465,
+    "unemployed": 371,
+    "rate": 4.7,
+    "lastLow": "11-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Alamosa",
+    "laborForce": 7434,
+    "employed": 7120,
+    "unemployed": 314,
+    "rate": 4.2,
+    "lastLow": "12-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Moffat",
+    "laborForce": 6945,
+    "employed": 6719,
+    "unemployed": 226,
+    "rate": 3.3,
+    "lastLow": "12-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Archuleta",
+    "laborForce": 6251,
+    "employed": 6074,
+    "unemployed": 177,
+    "rate": 2.8,
+    "lastLow": "10-2000",
+    "lastLowYear": 2000
+  },
+  {
+    "county": "Las Animas",
+    "laborForce": 5969,
+    "employed": 5666,
+    "unemployed": 303,
+    "rate": 5.1,
+    "lastLow": "11-2008",
+    "lastLowYear": 2008
+  },
+  {
+    "county": "Prowers",
+    "laborForce": 5942,
+    "employed": 5721,
+    "unemployed": 221,
+    "rate": 3.7,
+    "lastLow": "12-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Clear Creek",
+    "laborForce": 5581,
+    "employed": 5403,
+    "unemployed": 178,
+    "rate": 3.2,
+    "lastLow": "5-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Yuma",
+    "laborForce": 5197,
+    "employed": 5084,
+    "unemployed": 113,
+    "rate": 2.2,
+    "lastLow": "5-2002",
+    "lastLowYear": 2002
+  },
+  {
+    "county": "San Miguel",
+    "laborForce": 5069,
+    "employed": 4954,
+    "unemployed": 115,
+    "rate": 2.3,
+    "lastLow": "8-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Rio Grande",
+    "laborForce": 4903,
+    "employed": 4657,
+    "unemployed": 246,
+    "rate": 5,
+    "lastLow": "12-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Lake",
+    "laborForce": 4348,
+    "employed": 4212,
+    "unemployed": 136,
+    "rate": 3.1,
+    "lastLow": "4-2006",
+    "lastLowYear": 2006
+  },
+  {
+    "county": "Kit Carson",
+    "laborForce": 4165,
+    "employed": 4076,
+    "unemployed": 89,
+    "rate": 2.1,
+    "lastLow": "5-2001",
+    "lastLowYear": 2001
+  },
+  {
+    "county": "Conejos",
+    "laborForce": 3612,
+    "employed": 3451,
+    "unemployed": 161,
+    "rate": 4.5,
+    "lastLow": "10-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Gilpin",
+    "laborForce": 3382,
+    "employed": 3295,
+    "unemployed": 87,
+    "rate": 2.6,
+    "lastLow": "12-2000",
+    "lastLowYear": 2000
+  },
+  {
+    "county": "Saguache",
+    "laborForce": 3088,
+    "employed": 2898,
+    "unemployed": 190,
+    "rate": 6.2,
+    "lastLow": "12-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Rio Blanco",
+    "laborForce": 2860,
+    "employed": 2732,
+    "unemployed": 128,
+    "rate": 4.5,
+    "lastLow": "1-2009",
+    "lastLowYear": 2009
+  },
+  {
+    "county": "Washington",
+    "laborForce": 2808,
+    "employed": 2746,
+    "unemployed": 62,
+    "rate": 2.2,
+    "lastLow": "5-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Phillips",
+    "laborForce": 2447,
+    "employed": 2400,
+    "unemployed": 47,
+    "rate": 1.9,
+    "lastLow": "12-1999",
+    "lastLowYear": 1999
+  },
+  {
+    "county": "Huerfano",
+    "laborForce": 2331,
+    "employed": 2188,
+    "unemployed": 143,
+    "rate": 6.1,
+    "lastLow": "10-2008",
+    "lastLowYear": 2008
+  },
+  {
+    "county": "Ouray",
+    "laborForce": 2302,
+    "employed": 2232,
+    "unemployed": 70,
+    "rate": 3,
+    "lastLow": "10-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Dolores",
+    "laborForce": 2213,
+    "employed": 2178,
+    "unemployed": 35,
+    "rate": 1.6,
+    "lastLow": "New Low",
+    "lastLowYear": "New low"
+  },
+  {
+    "county": "Lincoln",
+    "laborForce": 2129,
+    "employed": 2070,
+    "unemployed": 59,
+    "rate": 2.8,
+    "lastLow": "10-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Custer",
+    "laborForce": 2031,
+    "employed": 1977,
+    "unemployed": 54,
+    "rate": 2.7,
+    "lastLow": "5-2000",
+    "lastLowYear": 2000
+  },
+  {
+    "county": "Baca",
+    "laborForce": 1970,
+    "employed": 1937,
+    "unemployed": 33,
+    "rate": 1.7,
+    "lastLow": "12-1998",
+    "lastLowYear": 1998
+  },
+  {
+    "county": "Bent",
+    "laborForce": 1745,
+    "employed": 1690,
+    "unemployed": 55,
+    "rate": 3.2,
+    "lastLow": "4-2000",
+    "lastLowYear": 2000
+  },
+  {
+    "county": "Costilla",
+    "laborForce": 1608,
+    "employed": 1525,
+    "unemployed": 83,
+    "rate": 5.2,
+    "lastLow": "10-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Crowley",
+    "laborForce": 1265,
+    "employed": 1215,
+    "unemployed": 50,
+    "rate": 4,
+    "lastLow": "10-1999",
+    "lastLowYear": 1999
+  },
+  {
+    "county": "Sedgwick",
+    "laborForce": 1155,
+    "employed": 1121,
+    "unemployed": 34,
+    "rate": 2.9,
+    "lastLow": "12-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Cheyenne",
+    "laborForce": 1063,
+    "employed": 1037,
+    "unemployed": 26,
+    "rate": 2.4,
+    "lastLow": "9-2008",
+    "lastLowYear": 2008
+  },
+  {
+    "county": "Jackson",
+    "laborForce": 871,
+    "employed": 847,
+    "unemployed": 24,
+    "rate": 2.8,
+    "lastLow": "11-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Kiowa",
+    "laborForce": 817,
+    "employed": 800,
+    "unemployed": 17,
+    "rate": 2.1,
+    "lastLow": "9-2007",
+    "lastLowYear": 2007
+  },
+  {
+    "county": "Hinsdale",
+    "laborForce": 532,
+    "employed": 518,
+    "unemployed": 14,
+    "rate": 2.6,
+    "lastLow": "12-2008",
+    "lastLowYear": 2008
+  },
+  {
+    "county": "San Juan",
+    "laborForce": 527,
+    "employed": 510,
+    "unemployed": 17,
+    "rate": 3.2,
+    "lastLow": "Rising",
+    "lastLowYear": "Rising"
+  },
+  {
+    "county": "Mineral",
+    "laborForce": 423,
+    "employed": 410,
+    "unemployed": 13,
+    "rate": 3.1,
+    "lastLow": "Rising",
+    "lastLowYear": "Rising"
+  }
+]
